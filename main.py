f81b01b0a2666214937bfc9aa905efad9cdc6e7e
AlexKent89 <137422029+AlexKent89@users.noreply.github.com>
2023-07-10 18:30:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
+#Belgium string
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+
 hyphens = '-' * len(Belgium)
 print(hyphens)
+
+#Change , to : using .replace
 belgium_colons = Belgium.replace(',', ':')
 print(belgium_colons)
 
+#Created an integer for Belgium value
 fields = Belgium.split(',')
 a = int(fields[1])
-print(a)
 
+#Created an integer for Brussel's value
 b = int(fields[3])
-print(b)
 
+#created a numerical string to add the two values together
 c = a + b
 print(c)
